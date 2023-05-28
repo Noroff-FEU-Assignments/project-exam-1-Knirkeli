9f739cfe276811126a0c244d6cf5267154769420
Eirik Kirkeli <115043925+Knirkeli@users.noreply.github.com>
2023-05-28 22:24:07 +0200

    Weedwacking

--- js/post.js
+++ js/post.js
@@ -1,32 +1,3 @@
-// const apiUrl = "https://knirkefridesign.no/blogtroll/wp-json/wp/v2/";
-
-// const urlParams = new URLSearchParams(window.location.search);
-// const postId = urlParams.get("id");
-
-// if (postId) {
-//   fetch(`${apiUrl}posts/${postId}`)
-//     .then((response) => response.json())
-//     .then((post) => {
-//       const headerElement = document.querySelector(".single__post--header");
-//       headerElement.textContent = post.title.rendered;
-//       const textElement = document.querySelector(".single__post--text");
-//       const parser = new DOMParser();
-//       const postContent = parser.parseFromString(
-//         post.content.rendered,
-//         "text/html"
-//       );
-//       textElement.innerHTML = postContent.body.innerHTML;
-//       const postImage = postContent.querySelector("img");
-//       if (postImage) {
-//         const imageElement = document.querySelector(".single__post--img");
-//         imageElement.src = postImage.src;
-//         imageElement.alt = postImage.alt;
-//       }
-//       const dateElement = document.querySelector(".single__post--date");
-//       dateElement.textContent = new Date(post.date).toLocaleDateString();
-//     });
-// }
-
 const apiUrl = "https://knirkefridesign.no/blogtroll/wp-json/wp/v2/";
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -54,7 +25,6 @@ if (postId) {
       const dateElement = document.querySelector(".single__post--date");
       dateElement.textContent = new Date(post.date).toLocaleDateString();
 
-      // Change the title
       const titleElement = document.querySelector("title");
       const descriptionElement = document.querySelector(
         'meta[name="description"]'
@@ -68,23 +38,3 @@ if (postId) {
       );
     });
 }
-
-// const commentForm = document.querySelector("#comment__form");
-// commentForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const comment = event.target.comment.value;
-//   fetch(`${apiUrl}comments`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       post: postId,
-//       content: comment,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // update page to display new comment
-//     });
-// });
